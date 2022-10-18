4d259c5dabef37a70fda12e63948580669525b53
summydev <adegbitesumayah@gmail.com>
2022-10-18 08:54:57 +0100

    commit

--- lib/features/profile/personal_details_screen.dart
+++ lib/features/profile/personal_details_screen.dart
@@ -1,2 +1,18 @@
 import 'package:flutter/material.dart';
 
+
+class PersonalDetails extends StatefulWidget {
+  const PersonalDetails({ Key? key }) : super(key: key);
+
+  @override
+  State<PersonalDetails> createState() => _PersonalDetailsState();
+}
+
+class _PersonalDetailsState extends State<PersonalDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
