3babde4ec50304e51492def23f91f161bd2a3209
summydev <adegbitesumayah@gmail.com>
2022-10-20 16:05:24 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyAppState extends State<MyApp> {
       //initialRoute: '/',
       home:
           //more
-          PersonalDetails(authEmail: '',),
+          AuthEmailScreen(),
       // routes: {
       //   '/': (context) => AuthEmailScreen(),
       //   '/api/email/signup': (context) => EmailSignup(
