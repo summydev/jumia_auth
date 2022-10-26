71fbe38f3650bc99c192951b8a0a09cf14c6b662
summydev <adegbitesumayah@gmail.com>
2022-10-26 12:06:25 +0100

    commit

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String uri = 'http://192.168.43.216:4000';
+String uri = 'http://192.168.0.116:4000';
 
 class GlobalVariables {
   // static const appBarGradient = LinearGradient(
